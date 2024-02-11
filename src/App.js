d2906daa1b9a78b3199c893ba81592ce0846e794
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2024-02-11 14:50:59 +0100

    creat components folder for search bar and list of countries, fetch api all countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import CountryCard from "./components/CountryCard";
+import CountrySearchBar from "./components/CountrySearchBar";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
+
+  //FETCH ALL COUNTRIES FROM THE API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        if (Array.isArray(data) && data.length > 0) {
+          const sortedData = data.sort((a, b) =>
+            a.name.common.localeCompare(b.name.common)
+          );
+          setCountries(sortedData);
+        } else {
+          console.error("Invalid data structure:", data);
+        }
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("countries", countries);
+
+  return (
+    <div className="countryWrapper">
+      <CountrySearchBar />
+      <CountryCard />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/CountrySearchBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountrySearchBar = () => {
+  return <div>CountrySearchBar</div>;
+};
+
+export default CountrySearchBar;
